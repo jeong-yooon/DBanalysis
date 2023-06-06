23fc9bc80a823b8015c492935fbff2ebe5fe6b4c
jeong-yooon <102950984+jeong-yooon@users.noreply.github.com>
2023-06-06 18:35:52 +0900

    Delete 미니프로젝트7/mini7_zinfin directory

--- 미니프로젝트7/mini7_zinfin/selfchatgpt/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'selfchat'
-urlpatterns = [
-    path('', views.index, name= 'index'),
-    path('chat', views.chat, name='chat')
-]
-
